cacf4df6e6db9f30f81c5d1df79456a891d32f7a
Danguole Luzaite <danguole.luzaite@gmail.com>
2022-02-14 01:35:16 +0100

    création du composant TheNavigation, modifications des fichiers.

---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <v-app-bar app color="orange darken-4" elevation="2" dark class="d-flex justify-center mb-6">
+      <v-img max-height="115" max-width="170"  
+      src="@/assets/logos/icon-left-font-monochrome-white.png"
+      ></v-img>
+      <v-toolbar-title  class="ml-5">Page de connexion/inscription</v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'TheNavigation'
+};
+</script>
